fdb86c131e940815d330c30d613fdd5ad8ad2ac9
futuretxzt <101665812+futuretxzt@users.noreply.github.com>
2022-05-04 20:48:53 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var s1,s2,s3
+
+
+function setup() {
+  createCanvas(400,400);
+s1 = new Student("Bob",14,9)
+s2 = new Student("joseph",15,10)
+s3 = new Student("Steve",13,8)
+s1.display()
+s2.display()
+s3.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
